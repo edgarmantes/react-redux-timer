b319866e51d5939c774f0591757fd65e8b6c9a95
Edgar Mantes <edgar.mantes11@gmail.com>
2017-03-19 21:17:56 -0500

    Added the carousel html/css to iphone6 view

--- js/components/call.js
+++ js/components/call.js
@@ -13,7 +13,47 @@ var Call = React.createClass({
 	        <div className="callContainer row">
 	        	<div className="call-first  call-div">
 	        		<div className="call-fixbackground-carosoul">
-	        		<img className="call-timer-img hidden" src="../images/timer.png" />
+	        			<img className="call-timer-img hidden" src="../images/timer.png" />
+	        			<div className="call-carousel-cont">
+
+							<input type="radio" id="1" name="activator" defaultChecked="checked" className="carousel__activator visuallyhidden"/>
+							<input type="radio" id="2" name="activator" className="carousel__activator visuallyhidden"/>
+							<input type="radio" id="3" name="activator" className="carousel__activator visuallyhidden"/>
+
+
+							<div className="carousel__controls">
+								<label htmlFor="3" className="carousel__control carousel__control--backward">backward3</label>    
+								<label htmlFor="2" className="carousel__control carousel__control--forward">forward2</label>
+							</div>
+							              
+							<div className="carousel__controls">
+								<label htmlFor="1" className="carousel__control carousel__control--backward">backward1</label>    
+								<label htmlFor="3" className="carousel__control carousel__control--forward">forward3</label>
+							</div>
+							              
+							<div className="carousel__controls">
+								<label htmlFor="2" className="carousel__control carousel__control--backward">backward2</label>    
+								<label htmlFor="1" className="carousel__control carousel__control--forward">forward1</label>
+							</div>
+							              
+
+							<li className="carousel__slide">
+								<h1>1</h1>
+							</li>
+							<li className="carousel__slide">
+								<h1>2</h1>
+							</li>
+							<li className="carousel__slide">
+								<h1>3</h1>
+							</li>
+							              
+
+							<div className="carousel__indicators">
+								<label htmlFor="1" className="carousel__indicator"></label>
+								<label htmlFor="2" className="carousel__indicator"></label>
+								<label htmlFor="3" className="carousel__indicator"></label>
+							</div>
+	        			</div>
 	        		</div>
 		        	<div className="enter-btns call-btns col-6">
 		            	<Link to="signin" ><button className="signIn">Sign In</button></Link>
