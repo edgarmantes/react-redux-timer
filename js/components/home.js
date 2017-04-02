a21cc999aa96cb5eaf9a23e42e08ce8b7e5aeb43
Edgar Mantes <edgar.mantes11@gmail.com>
2017-04-02 14:54:14 -0500

    Added the welcome tag and fixed the ajax call for all projects

--- js/components/home.js
+++ js/components/home.js
@@ -10,10 +10,10 @@ var actions = require('../actions/index');
 var Home = React.createClass({
 
 	componentDidMount: function(){
+		var username = this.props.username || localStorage.username
+		// var localArray = localStorage.getItem('TimerProjectArray');
 
-		var localArray = localStorage.getItem('TimerProjectArray');
-
-		this.props.dispatch(actions.getCards(localArray))
+		this.props.dispatch(actions.getCards(username))
 	},
 
 	createCard: function(event){
@@ -27,14 +27,14 @@ var Home = React.createClass({
 	},	
 		
 	render: function(props){
-
+		var username = this.props.username || localStorage.username;
 		return (
 			<div className="navigation">
 				<input type="checkbox" id="menuToggle" />
 				<label htmlFor="menuToggle" className="menu-icon"><img /></label>
 				<header>
-
-					<Link to="/home"><div className="timetracker">Time<img  className="clock" src="https://cdn2.iconfinder.com/data/icons/time-and-date-1/48/Clock-128.png" />Tracker</div></Link>
+					<Link to="/home"><div className="timetracker">Time<img  className="clock" src="../images/watch.png" />Tracker</div></Link>
+					<p className="home-username">Welcome: {username}</p>
 				</header>
 				<nav className="menu">
 					<h4 className="sidebar-header">New Card Form</h4>
@@ -57,7 +57,8 @@ var Home = React.createClass({
 
 var mapStateToProps = function(state, props){
 	return {
-		cards: state.cards
+		cards: state.cards,
+		username: state.username
 	}
 };
 
